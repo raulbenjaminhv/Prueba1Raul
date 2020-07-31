a8be519804f47ff2a27ed0602e343b6bf98b035d
negroletti <negroletti@gmail.com>
2020-07-31 01:14:09 -0400

    arregle la wea de codigo

--- prueba1.py
+++ prueba1.py
@@ -1,6 +1,8 @@
 ### Introducir un numero por teclado y decir si es par o impar
 num = int(input('Introduzca un numero: '))
-ifnum % 2 == 0:
-print('Par')
+if num % 2 == 0:
+    print('Par')
 else:
-print('Impar')
+    print('Impar')
+
+#Hola raul <3
